d098dc615bf0235f4f1238943044251be6388644
EFM2 <159607675+EFM2@users.noreply.github.com>
2024-02-10 08:36:13 +0100

    Delete src directory

--- src/actions/action.js
+++
@@ -1,9 +0,0 @@
-export const Incrementer=(step)=>{
-    return {type:'Incrementer',payLoad:step}
-}
-export const Decrementer=()=>{
-    return {type:'Decrementer'}
-}
-export const Reset=()=>{
-    return {type:'Reset'}
-}
